3231796b6f0dbc1f8e3dbc805c11a177a56e4537
Yarden Admon <yardenadmon@gmail.com>
2023-04-23 20:31:56 +0300

    commits for all the progress with Lydia

---
+++ Assets/Scripts/LoadedAsset.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[System.Serializable]
+public class LoadedAsset
+{
+    [SerializeField] int delayTime = 1000;
+    [SerializeField] string assetID;
+
+    // גורם חיצוני יכול לחכות על הפונקציה שהיא תסתיים
+    public async Task Load(CancellationToken cancellationToken)
+    {
+        await Task.Delay(delayTime, cancellationToken);
+    }
+
+    public async Task LoadFromResources(CancellationToken cancellationToken)
+    {
+        var request = Resources.LoadAsync(assetID);
+        while (!request.isDone)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
+            await Task.Yield();
+        }
+        GameObject.Instantiate(request.asset);
+    }
+}
